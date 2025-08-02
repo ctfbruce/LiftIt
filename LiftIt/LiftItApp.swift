d719b4cb3d433d3e34875c091ba482af7d7b8717
ctfbruce <mattlitheodor@gmail.com>
2025-08-02 16:09:29 +0530

    Initial Commit

---
+++ LiftIt/LiftItApp.swift
@@ -0,0 +1,20 @@
+//
+//  LiftItApp.swift
+//  LiftIt
+//
+//  Created by Theodor Mattli on 02.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct LiftItApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
